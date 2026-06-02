3a172694ccf46d02756cc537c48998831e05dbaf
Amir.Momen <amir.momen69@gmail.com>
2026-06-02 10:42:28 +0330

    edit freeRTOS get task name and stack size and stack base

--- cm_backtrace/cm_backtrace.h
+++ cm_backtrace/cm_backtrace.h
@@ -29,11 +29,14 @@
 #ifndef _CORTEXM_BACKTRACE_H_
 #define _CORTEXM_BACKTRACE_H_
 
-#include "cmb_def.h"
+#include <stdint.h>
+#include <stdlib.h>
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 
 void cm_backtrace_init(const char *firmware_name, const char *hardware_ver, const char *software_ver);
 void cm_backtrace_firmware_info(void);
--- cm_backtrace/cmb_def.h
+++ cm_backtrace/cmb_def.h
@@ -29,7 +29,7 @@
 #ifndef _CMB_DEF_H_
 #define _CMB_DEF_H_
 
-#include <cmb_cfg.h>
+#include "cmb_cfg.h"
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -177,15 +177,9 @@ struct cmb_hard_fault_regs{
     union {
       unsigned int value;
       struct {
-#if (CMB_CPU_PLATFORM_TYPE == CMB_CPU_ARM_CORTEX_M33)
-        unsigned int IPSR : 9;           // Interrupt Program Status register (IPSR)
-        unsigned int EPSR : 18;          // Execution Program Status register (EPSR)
-        unsigned int APSR : 5;           // Application Program Status register (APSR)
-#else
         unsigned int IPSR : 8;           // Interrupt Program Status register (IPSR)
         unsigned int EPSR : 19;          // Execution Program Status register (EPSR)
         unsigned int APSR : 5;           // Application Program Status register (APSR)
-#endif
       } bits;
     } psr;                               // Program status register.
   } saved;
@@ -343,10 +337,87 @@ if (!(EXPR))                                                                   \
     #elif (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_UCOSIII)
         #include <os.h>
     #elif (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_FREERTOS)
-        #include <FreeRTOS.h>  
-        extern uint32_t *vTaskStackAddr(void);/* need to modify the FreeRTOS/tasks source code */
-        extern uint32_t vTaskStackSize(void);
-        extern char * vTaskName(void);
+		#include "FreeRTOS.h"
+		#include "task.h"
+		/*
+		 * Task control block.  A task control block (TCB) is allocated for each task,
+		 * and stores task state information, including a pointer to the task's context
+		 * (the task's run time environment, including register values)
+		 */
+		typedef struct freertostskTaskControlBlock
+		{
+			volatile StackType_t	*pxTopOfStack;	/*< Points to the location of the last item placed on the tasks stack.  THIS MUST BE THE FIRST MEMBER OF THE TCB STRUCT. */
+
+			#if ( portUSING_MPU_WRAPPERS == 1 )
+				xMPU_SETTINGS	xMPUSettings;		/*< The MPU settings are defined as part of the port layer.  THIS MUST BE THE SECOND MEMBER OF THE TCB STRUCT. */
+			#endif
+
+			ListItem_t			xStateListItem;	/*< The list that the state list item of a task is reference from denotes the state of that task (Ready, Blocked, Suspended ). */
+			ListItem_t			xEventListItem;		/*< Used to reference a task from an event list. */
+			UBaseType_t			uxPriority;			/*< The priority of the task.  0 is the lowest priority. */
+			StackType_t			*pxStack;			/*< Points to the start of the stack. */
+			char				pcTaskName[ configMAX_TASK_NAME_LEN ];/*< Descriptive name given to the task when created.  Facilitates debugging only. */ /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
+
+			#if ( ( portSTACK_GROWTH > 0 ) || ( configRECORD_STACK_HIGH_ADDRESS == 1 ) )
+				StackType_t		*pxEndOfStack;		/*< Points to the highest valid address for the stack. */
+			#endif
+
+			#if ( portCRITICAL_NESTING_IN_TCB == 1 )
+				UBaseType_t		uxCriticalNesting;	/*< Holds the critical section nesting depth for ports that do not maintain their own count in the port layer. */
+			#endif
+
+			#if ( configUSE_TRACE_FACILITY == 1 )
+				UBaseType_t		uxTCBNumber;		/*< Stores a number that increments each time a TCB is created.  It allows debuggers to determine when a task has been deleted and then recreated. */
+				UBaseType_t		uxTaskNumber;		/*< Stores a number specifically for use by third party trace code. */
+			#endif
+
+			#if ( configUSE_MUTEXES == 1 )
+				UBaseType_t		uxBasePriority;		/*< The priority last assigned to the task - used by the priority inheritance mechanism. */
+				UBaseType_t		uxMutexesHeld;
+			#endif
+
+			#if ( configUSE_APPLICATION_TASK_TAG == 1 )
+				TaskHookFunction_t pxTaskTag;
+			#endif
+
+			#if( configNUM_THREAD_LOCAL_STORAGE_POINTERS > 0 )
+				void			*pvThreadLocalStoragePointers[ configNUM_THREAD_LOCAL_STORAGE_POINTERS ];
+			#endif
+
+			#if( configGENERATE_RUN_TIME_STATS == 1 )
+				uint32_t		ulRunTimeCounter;	/*< Stores the amount of time the task has spent in the Running state. */
+			#endif
+
+			#if ( configUSE_NEWLIB_REENTRANT == 1 )
+				/* Allocate a Newlib reent structure that is specific to this task.
+				Note Newlib support has been included by popular demand, but is not
+				used by the FreeRTOS maintainers themselves.  FreeRTOS is not
+				responsible for resulting newlib operation.  User must be familiar with
+				newlib and must provide system-wide implementations of the necessary
+				stubs. Be warned that (at the time of writing) the current newlib design
+				implements a system-wide malloc() that must be provided with locks. */
+				struct	_reent xNewLib_reent;
+			#endif
+
+			#if( configUSE_TASK_NOTIFICATIONS == 1 )
+				volatile uint32_t ulNotifiedValue;
+				volatile uint8_t ucNotifyState;
+			#endif
+
+			/* See the comments above the definition of
+			tskSTATIC_AND_DYNAMIC_ALLOCATION_POSSIBLE. */
+			#if( tskSTATIC_AND_DYNAMIC_ALLOCATION_POSSIBLE != 0 ) /*lint !e731 Macro has been consolidated for readability reasons. */
+				uint8_t	ucStaticallyAllocated; 		/*< Set to pdTRUE if the task is a statically allocated to ensure no attempt is made to free the memory. */
+			#endif
+
+			#if( INCLUDE_xTaskAbortDelay == 1 )
+				uint8_t ucDelayAborted;
+			#endif
+
+		} freertosTskTCB;
+
+
+typedef freertosTskTCB FreeRTOS_TCB_t;
     #elif (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_RTX5)
         #include "rtx_os.h"
     #elif (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_THREADX)
